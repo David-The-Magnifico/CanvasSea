2dd1175ac5eb77800eedabbbed7ffde8b78d9a8c
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-05 18:02:23 +0100

    Revert "Artwork sale build"
    This reverts commit b6b7c17caca70a42b3c245eae59a403a1e533c9a.

--- src/main/java/africa/semicolon/CanvasSea/Data/Repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package africa.semicolon.CanvasSea.Data.Repository;
-
-import africa.semicolon.CanvasSea.Data.Model.Admin;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AdminRepository extends MongoRepository<Admin, String> {
-    Admin findByUsername(String username);
-}
