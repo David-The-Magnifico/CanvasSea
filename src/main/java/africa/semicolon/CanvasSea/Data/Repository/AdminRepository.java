b6b7c17caca70a42b3c245eae59a403a1e533c9a
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-05 15:11:21 +0100

    Artwork sale build

---
+++ src/main/java/africa/semicolon/CanvasSea/Data/Repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package africa.semicolon.CanvasSea.Data.Repository;
+
+import africa.semicolon.CanvasSea.Data.Model.Admin;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AdminRepository extends MongoRepository<Admin, String> {
+    Admin findByUsername(String username);
+}
