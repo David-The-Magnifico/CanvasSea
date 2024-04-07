d61c5098e303f8ca4fb53492792f424be8c57b0b
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-07 22:21:09 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Services/EmailServiceImpl.java
+++ src/main/java/africa/semicolon/CanvasSea/Services/EmailServiceImpl.java
@@ -5,6 +5,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import africa.semicolon.CanvasSea.DTOs.Request.EmailRequest;
 import africa.semicolon.CanvasSea.DTOs.Response.EmailResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
