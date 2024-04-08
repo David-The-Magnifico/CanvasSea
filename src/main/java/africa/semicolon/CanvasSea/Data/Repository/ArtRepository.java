2b639ab19e11526e8b26c4e34895ac7514bea3a2
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-08 08:03:20 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Data/Repository/ArtRepository.java
+++ src/main/java/africa/semicolon/CanvasSea/Data/Repository/ArtRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ArtRepository extends MongoRepository<Art, String> {
-    Optional<Art> findById(String artId);
+    Optional<Art> findByArtist(String artId);
 
     Art findArtByIdAndArtist(String ArtId, Artist artistId);
     Art findArtByArtId(String artId);
