a29629cce50e93f46be6f6917944a2272e9d9ee9
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-08 05:38:14 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Services/BuyerService.java
+++ src/main/java/africa/semicolon/CanvasSea/Services/BuyerService.java
@@ -1,6 +1,7 @@
 package africa.semicolon.CanvasSea.Services;
 
 import africa.semicolon.CanvasSea.DTOs.Request.LoginRequest;
+import africa.semicolon.CanvasSea.DTOs.Request.PurchaseArtRequest;
 import africa.semicolon.CanvasSea.DTOs.Request.RegisterRequest;
 import africa.semicolon.CanvasSea.Data.Model.Art;
 import africa.semicolon.CanvasSea.Data.Model.Buyer;
@@ -13,4 +14,7 @@ public interface BuyerService {
     Buyer register(RegisterRequest registerRequest);
 
     void login(LoginRequest loginRequest);
+
+    void purchase(PurchaseArtRequest purchaseArtRequest);
+
 }
