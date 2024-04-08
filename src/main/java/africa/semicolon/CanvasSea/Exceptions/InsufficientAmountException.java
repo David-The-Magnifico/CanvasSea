0e9a1bbef4dbc8f36f409e2b1777b4bb07a1f28e
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-08 05:38:45 +0100

    Artwork sale build

---
+++ src/main/java/africa/semicolon/CanvasSea/Exceptions/InsufficientAmountException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.CanvasSea.Exceptions;
+
+public class InsufficientAmountException extends RuntimeException {
+    public InsufficientAmountException(String message) {
+        super(message);
+    }
+}
