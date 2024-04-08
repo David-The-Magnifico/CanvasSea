da235ad5432fcc2aabce7fb4ea86efbcc5a83786
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-08 05:37:41 +0100

    Artwork sale build

---
+++ src/main/java/africa/semicolon/CanvasSea/Exceptions/BuyerDoesNotExistException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.CanvasSea.Exceptions;
+
+public class BuyerDoesNotExistException extends RuntimeException {
+    public BuyerDoesNotExistException(String message) {
+        super(message);
+    }
+}
--- src/main/java/africa/semicolon/CanvasSea/Exceptions/BuyerExistException.java
+++
@@ -1,7 +0,0 @@
-package africa.semicolon.CanvasSea.Exceptions;
-
-public class BuyerExistException extends RuntimeException {
-    public BuyerExistException(String message) {
-        super(message);
-    }
-}
