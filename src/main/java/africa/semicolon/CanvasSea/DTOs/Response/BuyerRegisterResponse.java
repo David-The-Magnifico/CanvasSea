2abc96b0d45fb3d338cd88765d7ca2d4afc74997
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-08 05:38:04 +0100

    Artwork sale build

---
+++ src/main/java/africa/semicolon/CanvasSea/DTOs/Response/BuyerRegisterResponse.java
@@ -0,0 +1,9 @@
+package africa.semicolon.CanvasSea.DTOs.Response;
+
+import lombok.Data;
+import africa.semicolon.CanvasSea.Data.Model.Buyer;
+
+@Data
+public class BuyerRegisterResponse {
+    private String message;
+}
