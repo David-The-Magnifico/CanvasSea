977ec6e437140322446ae3ca023ddc728d2e09ac
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-08 05:37:05 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Services/ArtServiceImpl.java
+++ src/main/java/africa/semicolon/CanvasSea/Services/ArtServiceImpl.java
@@ -2,18 +2,19 @@ package africa.semicolon.CanvasSea.Services;
 
 import africa.semicolon.CanvasSea.DTOs.Request.DisplayArtRequest;
 import africa.semicolon.CanvasSea.Data.Model.*;
-import africa.semicolon.CanvasSea.Data.Repository.ArtistRepository;
-import africa.semicolon.CanvasSea.Exceptions.ArtNotFoundException;
+import africa.semicolon.CanvasSea.Data.Repository.ArtRepository;
+import africa.semicolon.CanvasSea.Exceptions.*;
 import africa.semicolon.CanvasSea.Utils.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class ArtServiceImpl {
+public class ArtServiceImpl implements ArtService {
     @Autowired
-    private ArtistRepository artistRepository;
+    private ArtRepository artRepository;
     @Override
     public Art create(DisplayArtRequest displayArtRequest, Artist foundArtist) {
         Art art = Mapper.mapArt(displayArtRequest, foundArtist);
@@ -29,7 +30,7 @@ public class ArtServiceImpl {
     }
     @Override
     public Art findArt(String artId) {
-        Art art = artRepository.findArtById(artId);
+        Art art = artRepository.findArtByArtId(artId);
         return art;
     }
     @Override
@@ -46,7 +47,7 @@ public class ArtServiceImpl {
     }
     @Override
     public Art findAArt(String artId) {
-        return artRepository.findByArtId(artId).get();
+        return artRepository.findById(artId).get();
     }
     @Override
     public void removeAArt(String artId) {
@@ -55,5 +56,15 @@ public class ArtServiceImpl {
             artRepository.delete(findAArt(artId));
         }
         throw new ArtNotFoundException("Art not found");
+    }
+
+    @Override
+    public void saveArt(Art art) {
+        artRepository.save(art);
+    }
+
+    @Override
+    public Optional<Art> findArtById(String artId) {
+        return artRepository.findById(artId);
     }
 }
