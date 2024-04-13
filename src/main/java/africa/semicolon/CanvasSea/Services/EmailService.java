5159e51034ccbc0dd37d70635391adf2c21543c6
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-13 11:50:37 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Services/EmailService.java
+++
@@ -1,8 +0,0 @@
-package africa.semicolon.CanvasSea.Services;
-
-import africa.semicolon.CanvasSea.DTOs.Request.EmailRequest;
-import africa.semicolon.CanvasSea.DTOs.Response.EmailResponse;
-
-public interface EmailService {
-    EmailResponse sendMailMessage(EmailRequest emailRequest);
-}
