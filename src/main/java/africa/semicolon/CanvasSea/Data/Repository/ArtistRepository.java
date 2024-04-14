08520f563d4a091794f2afe5f0cf9c696cd20911
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-14 23:53:54 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Data/Repository/ArtistRepository.java
+++ src/main/java/africa/semicolon/CanvasSea/Data/Repository/ArtistRepository.java
@@ -1,12 +1,14 @@
 package africa.semicolon.CanvasSea.Data.Repository;
 
+import africa.semicolon.CanvasSea.Data.Model.Art;
 import africa.semicolon.CanvasSea.Data.Model.Artist;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface ArtistRepository extends MongoRepository<Artist, String> {
-    Optional<Artist> findByUsername(String artistName);
-    Optional<Artist> findByEmail(String email);
+    Artist findByUsername(String artistName);
+    Artist findByEmail(String email);
 
+    List<Art> findAllArt(String username, String email);
 }
