4d8f97306faf9dcf26c12df26ddeb8a96e894c97
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-14 23:55:41 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Services/ArtServiceImpl.java
+++ src/main/java/africa/semicolon/CanvasSea/Services/ArtServiceImpl.java
@@ -1,9 +1,10 @@
 package africa.semicolon.CanvasSea.Services;
 
 import africa.semicolon.CanvasSea.DTOs.Request.DisplayArtRequest;
-import africa.semicolon.CanvasSea.Data.Model.*;
+import africa.semicolon.CanvasSea.Data.Model.Art;
+import africa.semicolon.CanvasSea.Data.Model.Artist;
 import africa.semicolon.CanvasSea.Data.Repository.ArtRepository;
-import africa.semicolon.CanvasSea.Exceptions.*;
+import africa.semicolon.CanvasSea.Exceptions.ArtNotFoundException;
 import africa.semicolon.CanvasSea.Utils.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,47 +16,64 @@ import java.util.Optional;
 public class ArtServiceImpl implements ArtService {
     @Autowired
     private ArtRepository artRepository;
+
     @Override
     public Art create(DisplayArtRequest displayArtRequest, Artist foundArtist) {
         Art art = Mapper.mapArt(displayArtRequest, foundArtist);
         save(art);
         return art;
     }
+
     @Override
     public void save(Art art) {
         artRepository.save(art);
     }
+
     @Override
-    public  void delete(List<Art> arts) {artRepository.deleteAll(arts);
+    public void delete(List<Art> arts) {
+        artRepository.deleteAll(arts);
     }
+
     @Override
     public Art findArt(String artId) {
-        Art art = artRepository.findArtByArtId(artId);
+        Art art = artRepository.findArtById(artId);
         return art;
     }
+
     @Override
     public Long count() {
         return artRepository.count();
     }
+
     @Override
-    public List<Art> findArtsOwnedBy(String email) {
-        return artRepository.findArtsByArtist_Email(email);
+    public Art create(DisplayArtRequest displayArtRequest, Optional<Artist> foundArtist) {
+        return null;
     }
+
     @Override
     public List<Art> findAllArt() {
         return artRepository.findAll();
     }
+
     @Override
-    public Art findAArt(String artId) {
-        return artRepository.findById(artId).get();
+    public Art findAArt(String id) {
+        Optional<Art> optionalArt = artRepository.findById(id);
+        if (optionalArt.isPresent()) {
+            return optionalArt.get();
+        } else {
+            throw new ArtNotFoundException("Art not found");
+        }
     }
+
+
     @Override
     public void removeAArt(String artId) {
         Art art = findAArt(artId);
         if (art.isPublished()) {
-            artRepository.delete(findAArt(artId));
+            artRepository.delete(art);
+        } else {
+            throw new ArtNotFoundException("Art not found");
         }
-        throw new ArtNotFoundException("Art not found");
     }
 
     @Override
@@ -64,7 +82,7 @@ public class ArtServiceImpl implements ArtService {
     }
 
     @Override
-    public Optional<Art> findArtById(String artId) {
-        return artRepository.findById(artId);
+    public Art findArtById(String id) {
+        return artRepository.findArtById(id);
     }
 }
