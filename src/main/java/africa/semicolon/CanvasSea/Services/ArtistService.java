7089f5559e3cbba6191049f140863235928855fb
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-14 23:54:10 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Services/ArtistService.java
+++ src/main/java/africa/semicolon/CanvasSea/Services/ArtistService.java
@@ -4,12 +4,11 @@ import africa.semicolon.CanvasSea.DTOs.Request.*;
 import africa.semicolon.CanvasSea.Data.Model.*;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ArtistService {
     Artist register(RegisterRequest registerRequest);
 
-    Optional<Artist> findArtist(String email);
+    Artist findArtist(String email);
 
     void remove(String username, String email);
 
@@ -17,13 +16,11 @@ public interface ArtistService {
 
     Art displayArt(DisplayArtRequest displayArtRequest);
 
-    Optional<Artist> findArtistByEmail(String email);
+    Artist findArtistByEmail(String email);
 
     List<Art> findAllArt(String username, String email);
 
     Art findAArt(FindAArtRequest findAArtRequest);
 
     void removeAArt(RemoveAArtRequest removeAArtRequest);
-
-
 }
