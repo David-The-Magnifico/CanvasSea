194a146baab7bf5c4db2f69a01c7f4f44e67cb00
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-14 23:57:59 +0100

    Artwork sale build

---
+++ src/main/java/africa/semicolon/CanvasSea/Exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.CanvasSea.Exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
