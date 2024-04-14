78fc16bf4f3be7f7eae335ee5a4c32c2728aa391
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-14 23:55:56 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Data/Repository/BuyerRepository.java
+++ src/main/java/africa/semicolon/CanvasSea/Data/Repository/BuyerRepository.java
@@ -3,9 +3,7 @@ package africa.semicolon.CanvasSea.Data.Repository;
 import africa.semicolon.CanvasSea.Data.Model.Buyer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
 public interface BuyerRepository extends MongoRepository<Buyer, String> {
-    Optional<Buyer> findByUsername(String buyerName);
-    Optional<Buyer> findByEmail(String email);
+    Buyer findByUsername(String buyerName);
+    Buyer findByEmail(String email);
 }
