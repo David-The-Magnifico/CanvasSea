5dad86a65e6722488ca96cd77696cc904f5d9504
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-04-16 03:15:25 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/Main.java
+++ src/main/java/africa/semicolon/CanvasSea/Main.java
@@ -1,9 +1,5 @@
 package africa.semicolon.CanvasSea;
 
-import africa.semicolon.CanvasSea.Data.Model.Admin;
-import africa.semicolon.CanvasSea.Data.Repository.AdminRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
