2aba9b6301d30d841ecdf4a9b28b59131b681259
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-05-03 10:22:25 +0100

    Artwork sale build

--- src/main/java/africa/semicolon/CanvasSea/DTOs/Request/DisplayArtRequest.java
+++ src/main/java/africa/semicolon/CanvasSea/DTOs/Request/DisplayArtRequest.java
@@ -6,9 +6,9 @@ import java.math.BigDecimal;
 
 @Data
 public class DisplayArtRequest {
-    private String email;
     private String artName;
     private String description;
     private BigDecimal amount;
     private String artistUsername;
+    private String email;
 }
