de0329c6929ea55ed2547d62173b96cc97aabfe3
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-05-08 15:35:56 +0100

    ArtWork Sales build

--- src/main/java/africa/semicolon/CanvasSea/DTOs/Request/UploadRequest.java
+++ src/main/java/africa/semicolon/CanvasSea/DTOs/Request/UploadRequest.java
@@ -1,12 +1,15 @@
 package africa.semicolon.CanvasSea.DTOs.Request;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class UploadRequest {
     private String username;
     private String password;
     private String email;
     private String artId;
     private String artistId;
+
 }
