dea039f2159a4833d81d979608fa156a93462663
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-05-10 12:59:44 +0100

    integrated cloud dependencies for image upload and storage

--- src/main/java/africa/semicolon/CanvasSea/Services/cloud/CloudServices.java
+++ src/main/java/africa/semicolon/CanvasSea/Services/cloud/CloudServices.java
@@ -2,6 +2,10 @@ package africa.semicolon.CanvasSea.Services.cloud;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface CloudServices {
     String uploadImage(MultipartFile toUpload);
+
+    List<String> uploadMultipleImages(List<MultipartFile> toUpload);
 }
