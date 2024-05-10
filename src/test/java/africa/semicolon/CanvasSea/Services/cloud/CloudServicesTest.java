041fde75e2703eced6f16c88bf341a20bc957f71
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-05-10 13:00:21 +0100

    integrated cloud dependencies for image upload and storage

--- src/test/java/africa/semicolon/CanvasSea/Services/cloud/CloudServicesTest.java
+++ src/test/java/africa/semicolon/CanvasSea/Services/cloud/CloudServicesTest.java
@@ -13,6 +13,8 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -36,4 +38,37 @@ class CloudServicesTest {
             throw new RuntimeException("image upload fail",e);
         }
     }
+
+    @Test
+    void uploadMultipleImages() {
+        List<MultipartFile> files = new ArrayList<>();
+
+        Path path1 = Path.of(Validator.filePath + "sci-fi.jpeg");
+        Path path2 = Path.of(Validator.filePath + "Space.jpeg");
+        Path path3 = Path.of(Validator.filePath + "Tech.jpeg");
+
+        try {
+            InputStream inputStream1 = Files.newInputStream(path1);
+            MultipartFile file1 = new MockMultipartFile("image1", inputStream1);
+
+            InputStream inputStream2 = Files.newInputStream(path2);
+            MultipartFile file2 = new MockMultipartFile("image2", inputStream2);
+
+            InputStream inputStream3 = Files.newInputStream(path3);
+            MultipartFile file3 = new MockMultipartFile("image3", inputStream3);
+
+            files.add(file1);
+            files.add(file2);
+            files.add(file3);
+
+            List<String> uploadResponses = cloudServices.uploadMultipleImages(files);
+
+            assertNotNull(uploadResponses);
+            assertEquals(3, uploadResponses.size());
+
+        } catch (IOException e) {
+            throw new RuntimeException("image upload fail", e);
+        }
+    }
+
 }
