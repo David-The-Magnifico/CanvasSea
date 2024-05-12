63a456fbc82c1117359900792e939eac20acf542
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-05-12 17:43:14 +0100

    integrated Paystack payment Gateway for peer-to-peer transaction

--- src/main/java/africa/semicolon/CanvasSea/Data/Repository/BuyerRepository.java
+++ src/main/java/africa/semicolon/CanvasSea/Data/Repository/BuyerRepository.java
@@ -2,7 +2,9 @@ package africa.semicolon.CanvasSea.Data.Repository;
 
 import africa.semicolon.CanvasSea.Data.Model.Buyer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BuyerRepository extends MongoRepository<Buyer, String> {
     Buyer findByUsername(String buyerName);
     Buyer findByEmail(String email);
