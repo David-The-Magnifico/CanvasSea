5fd10f5bc5b4db7ee4c5f76ee0df8dff2079e444
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-05-12 17:45:16 +0100

    integrated Paystack payment Gateway for peer-to-peer transaction

---
+++ src/main/java/africa/semicolon/CanvasSea/Exceptions/PaymentVerificationException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.CanvasSea.Exceptions;
+
+public class PaymentVerificationException extends Exception {
+    public PaymentVerificationException(String message) {
+        super(message);
+    }
+}
