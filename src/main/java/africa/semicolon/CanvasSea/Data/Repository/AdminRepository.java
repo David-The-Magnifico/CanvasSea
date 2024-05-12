e1ee2890e52e13f52bc190f691899bcc82f11af4
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-05-12 17:42:26 +0100

    integrated Paystack payment Gateway for peer-to-peer transaction

--- src/main/java/africa/semicolon/CanvasSea/Data/Repository/AdminRepository.java
+++ src/main/java/africa/semicolon/CanvasSea/Data/Repository/AdminRepository.java
@@ -2,7 +2,9 @@ package africa.semicolon.CanvasSea.Data.Repository;
 
 import africa.semicolon.CanvasSea.Data.Model.Admin;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends MongoRepository<Admin, String> {
     Admin findByUsername(String admin);
 }
