56afeb3794bdc10907fc39aa184ebdb021c8b367
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-05-12 17:42:46 +0100

    integrated Paystack payment Gateway for peer-to-peer transaction

--- src/main/java/africa/semicolon/CanvasSea/Data/Repository/ArtistRepository.java
+++ src/main/java/africa/semicolon/CanvasSea/Data/Repository/ArtistRepository.java
@@ -1,12 +1,14 @@
 package africa.semicolon.CanvasSea.Data.Repository;
 
-import africa.semicolon.CanvasSea.Data.Model.Art;
 import africa.semicolon.CanvasSea.Data.Model.Artist;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface ArtistRepository extends MongoRepository<Artist, String> {
     Artist findByUsername(String artistName);
     Artist findByEmail(String email);
+    Optional<Artist> findById(String id);
 }
