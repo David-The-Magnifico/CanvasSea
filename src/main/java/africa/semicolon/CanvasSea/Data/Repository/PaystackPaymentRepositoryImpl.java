d492823bcf2d32c7e0cfeea30b70bb231f2e2760
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-06-04 11:02:36 +0100

    integrated Paystack payment Gateway for peer-to-peer transaction

--- src/main/java/africa/semicolon/CanvasSea/Data/Repository/PaystackPaymentRepositoryImpl.java
+++ src/main/java/africa/semicolon/CanvasSea/Data/Repository/PaystackPaymentRepositoryImpl.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaystackPaymentRepository extends MongoRepository<PaymentPaystack, String> {
+public interface PaystackPaymentRepositoryImpl extends MongoRepository<PaymentPaystack, String> {
 }
